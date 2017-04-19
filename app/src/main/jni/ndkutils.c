dec2a95e98499634c3ed8e82684dcca3eec2d100
yutianran <yutianran@isoubu.com>
2017-04-19 17:32:16 +0800

    删除了jni

--- app/src/main/jni/ndkutils.c
+++
@@ -1,9 +0,0 @@
-//
-// Created by 余天然 on 2017/4/19.
-//
-
-#include "com_soubu_jnidemo_JniUtils.h"
-
-JNIEXPORT jstring JNICALL Java_com_soubu_jnidemo_JniUtils_getStringFormC(JNIEnv *env, jobject obj) {
-    return (*env)->NewStringUTF(env, "这里是来自c的string");
-}
